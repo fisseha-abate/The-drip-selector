5d514f175a36138edc2d56ab2162896f48a74ac4
pbcurran <s3303005@student.rmit.edu.au>
2020-10-07 21:24:29 +1100

    classes

---
+++ Scripts/index.js
@@ -0,0 +1,7 @@
+// $(".clothing").on("click", function () {
+//   // Do stuff, get id of image perhaps?
+//   console.log("image clicked");
+//   var id = this.id;
+//   // or
+//   var id = $(this).attr("id");
+// });
